14268c3c6eae0af534dc94692580327b91fcc56d
lstmemery <matthew.emery44@gmail.com>
2023-03-11 14:02:49 -0800

    SOCS-O Automated

--- make_files.fish
+++ make_files.fish
@@ -1,3 +1,10 @@
 #! /usr/bin/fish
-quarto render notebooks/analysis_template.qmd -P data:../results/socs_o_tidy.feather -P model:../models/socs_o_one_factor.json -t docx --output socs_o_one_factor.docx
-mv socs_o_one_factor.docx output
+
+function render_docx
+    quarto render notebooks/analysis_template.qmd -P data:../results/$argv[1].feather -P model:../models/$argv[2].json -t docx --output $argv[2].docx
+    mv $argv[2].docx output
+end
+
+render_docx socs_o_tidy socs_o_one_factor
+render_docx socs_o_tidy socs_o_five_factor
+render_docx socs_o_tidy socs_o_hierarchical
